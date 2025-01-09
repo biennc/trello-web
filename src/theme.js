97cc957583c9ff1c032efb903576abffa819756a
biennc <145189115+biennc@users.noreply.github.com>
2025-01-09 20:40:32 +0700

    design boardbar (#2)

--- src/theme.js
+++ src/theme.js
@@ -27,6 +27,23 @@ const theme = extendTheme({
   },
   components: {
     // Name of the component
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          '*::-webkit-scrollbar': {
+            width: '8px',
+            Height: '8px'
+          },
+          '*::-webkit-scrollbar-thumb': {
+            backgroundColor: '#bdc3c7',
+            borderRadius: '8px'
+          },
+          '*::-webkit-scrollbar-thumb:hover': {
+            backgroundColor: '#00b894'
+          }
+        }
+      }
+    },
     MuiButton: {
       styleOverrides: {
         // Name of the slot
